797f70b97c8db023f61bfcb590d3a7ca988b148a
FOODOHOLIC <adarshkohli183@gmail.com>
2025-05-29 08:42:06 +0530

    new

--- Array/src/KadensAlgo.java
+++ Array/src/KadensAlgo.java
@@ -1,6 +1,23 @@
 import java.util.*;
 
 public class KadensAlgo {
+
+    public static int max(int[] arr, int n){
+        long maxi = Integer.MIN_VALUE;
+        long sum =0;
+
+        for (int i = 0; i < n; i++) {
+            sum +=arr[i];
+            if (sum>maxi){
+                maxi = sum;
+            }
+
+            if (sum<0){
+                sum =0;
+            }
+        }
+        return (int) maxi;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -9,22 +26,14 @@ public class KadensAlgo {
             int n = in.nextInt();
             int[] arr = new int[n];
 
-            long maxi = Integer.MIN_VALUE;
-            long sum =0;
-
             for (int i = 0; i < n; i++) {
                 arr[i] = in.nextInt();
-
-                sum +=arr[i];
-                if (sum>maxi){
-                    maxi = sum;
-                }
-
-                if (sum<0){
-                    sum =0;
-                }
             }
-            System.out.println(maxi);
+
+            int x = max(arr,n);
+            System.out.println(x);
         }
+
+
     }
 }

5fcbd9d4ab2d138592baf46cce83286cb2452136
FOODOHOLIC <adarshkohli183@gmail.com>
2025-06-08 08:24:24 +0530

    new

---
+++ Array/src/ReversePairs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ReversePairs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t= in.nextInt();
+        while (t-->0){
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+
+
+        }
+    }
+}

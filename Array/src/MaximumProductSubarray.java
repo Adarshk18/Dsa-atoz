f97eac4b20a4f56de62a67e3dd3d0676ebb753ed
FOODOHOLIC <adarshkohli183@gmail.com>
2025-06-08 10:43:27 +0530

    new

--- Array/src/MaximumProductSubarray.java
+++ Array/src/MaximumProductSubarray.java
@@ -10,8 +10,25 @@ public class MaximumProductSubarray {
             for (int i = 0; i < n; i++) {
                 arr[i] = in.nextInt();
             }
+            if (arr == null || n==0){
+                return;
+            }
+            int maxProd = arr[0];
+            int minProd = arr[0];
+            int result = arr[0];
+            for (int i = 1; i < n; i++) {
+                if (arr[i]<0){
+                    int temp = maxProd;
+                    maxProd = minProd;
+                    minProd = temp;
+                }
 
+                maxProd = Math.max(arr[i],maxProd*arr[i]);
+                minProd = Math.min(arr[i],minProd*arr[i]);
 
+                result = Math.max(result,maxProd);
+            }
+            System.out.println(result);
         }
     }
 }

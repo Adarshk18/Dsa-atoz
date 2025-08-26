1cf942ba666a8e047da8aecfc31812e31dccb1e1
Adarshk18 <adarshkohli183@gmail.com>
2025-08-26 09:36:00 +0530

    new

---
+++ Tree/src/FlattenTree.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+public class FlattenTree {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static Node buildTreeFromArray(int[] arr) {
+        if (arr.length == 0 || arr[0] == -1) return null;
+
+        Node root = new Node(arr[0]);
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int i = 1;
+
+        while (!q.isEmpty() && i < arr.length) {
+            Node curr = q.poll();
+
+            // Left child
+            if (arr[i] != -1) {
+                curr.left = new Node(arr[i]);
+                q.add(curr.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != -1) {
+                curr.right = new Node(arr[i]);
+                q.add(curr.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    public static void flatten(Node root){
+        Node curr = root;
+        while (curr!=null){
+            if (curr.left!=null){
+                Node prev = curr.left;
+                while (prev.right!=null){
+                    prev = prev.right;
+                }
+
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left =null;
+            }
+            curr = curr.right;
+        }
+    }
+    public static void printPreorder(Node root){
+        if (root==null) return;
+
+        System.out.print(root.data+ " ");
+        printPreorder(root.left);
+        printPreorder(root.right);
+    }
+
+    public static void printFlatten(Node root){
+        if (root==null) return;
+
+        System.out.print(root.data + " ");
+        printFlatten(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+
+        while (t-->0){
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
+            }
+
+            Node root = buildTreeFromArray(arr);
+
+            printPreorder(root);
+            System.out.println();
+
+            flatten(root);
+
+            printFlatten(root);
+            System.out.println();
+        }
+    }
+}

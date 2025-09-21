58a8c790dda550de3ff7a22ea46cc8ae7275a465
Adarshk18 <adarshkohli183@gmail.com>
2025-09-21 08:55:30 +0530

    new

---
+++ Graph/src/FloydWarshall.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class FloydWarshall {
+
+    public static void shortestDistance(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = (int) (1e9);
+                }
+                if (i == j) matrix[i][j] = 0;
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][k] != (int) 1e9 && matrix[k][j] != (int) 1e9) {
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][i] < 0) System.out.println("Negative cycle");
+            return;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == (int) (1e9)) {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        while (t-- > 0) {
+            int n = in.nextInt();
+
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = in.nextInt();
+                }
+            }
+            shortestDistance(matrix);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}

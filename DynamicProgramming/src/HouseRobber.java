dcf6abff5ccd54c4c4ba4475499ca8b4fb389ac6
Adarshk18 <adarshkohli183@gmail.com>
2025-10-20 09:19:30 +0530

    new

--- DynamicProgramming/src/HouseRobber.java
+++ DynamicProgramming/src/HouseRobber.java
@@ -1,42 +1,62 @@
 import java.util.*;
 
 public class HouseRobber {
-    public static int solve(int[] nums){
+
+    // Helper method to solve the linear version (House Robber I)
+    public static int solve(int[] nums) {
         int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
         int prev = nums[0];
         int prev2 = 0;
 
         for (int i = 1; i < n; i++) {
             int pick = nums[i];
-            if (i>1) pick += prev2;
+            if (i > 1) pick += prev2;
 
             int notPick = prev;
+            int curr = Math.max(pick, notPick);
 
-            int curr = Math.max(pick,notPick);
             prev2 = prev;
             prev = curr;
         }
         return prev;
     }
 
+    // Main function for circular houses
     public static int rob(int[] nums) {
         int n = nums.length;
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
+        if (n == 1) return nums[0];
 
-        if (n==1) return nums[0];
+        // Exclude first house (take 1...n-1)
+        int[] arr1 = Arrays.copyOfRange(nums, 1, n);
+        // Exclude last house (take 0...n-2)
+        int[] arr2 = Arrays.copyOfRange(nums, 0, n - 1);
 
-        for (int i = 0; i < n; i++) {
-            if (i!=0) arr1.add(arr[i]);
-            if (i!=n-1) arr2.add(arr[i])
+        return Math.max(solve(arr1), solve(arr2));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of test cases: ");
+        int t = sc.nextInt();
+
+        for (int test = 1; test <= t; test++) {
+            System.out.print("\nEnter number of houses: ");
+            int n = sc.nextInt();
+
+            int[] nums = new int[n];
+            System.out.println("Enter money in each house:");
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+
+            int result = rob(nums);
+            System.out.println("Maximum money that can be robbed: " + result);
         }
 
-        int ans1 = solve(arr1);
-        int ans2 = solve(arr2);
-
-        return Math.max(ans1,ans2);
-    }
-    public static void main(String[] args) {
-
+        sc.close();
     }
 }

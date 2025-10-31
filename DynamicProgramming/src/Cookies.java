1ceeddca7314ae018a7cd3293ea1f4e6b5b5cf9e
Adarshk18 <adarshkohli183@gmail.com>
2025-10-31 08:39:12 +0530

    new

--- DynamicProgramming/src/Cookies.java
+++ DynamicProgramming/src/Cookies.java
@@ -1,7 +1,44 @@
 import java.util.*;
 
 public class Cookies {
-    public static void main(String[] args) {
 
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int n = g.length;
+        int m = s.length;
+
+        int r = 0, l = 0;
+
+
+        while (r<n && l < m) {
+            if (g[r] <= s[l]) {
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int[] g = new int[n];
+            for (int i = 0; i < n; i++) {
+                g[i] = in.nextInt();
+            }
+
+            int m = in.nextInt();
+            int[] s = new int[m];
+            for (int i = 0; i < m; i++) {
+                s[i] = in.nextInt();
+            }
+
+            int ans = findContentChildren(g, s);
+            System.out.println(ans);
+        }
     }
 }

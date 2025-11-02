01e89fedccae9a263948cf78a52746a0039b07b0
Adarshk18 <adarshkohli183@gmail.com>
2025-11-02 09:33:07 +0530

    new

--- DynamicProgramming/src/LongestCommonSubsequence.java
+++ DynamicProgramming/src/LongestCommonSubsequence.java
@@ -6,30 +6,24 @@ public class LongestCommonSubsequence {
         int n = text1.length();
         int  m = text2.length();
 
-        int[][] dp = new int[n+1][m+1];
-
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
-        }
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
 
         for (int j = 0; j <= m; j++) {
-            dp[0][j] = 0;
+            prev[j] = 0;
         }
 
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
-        }
 
         for (int ind1 = 1; ind1 <= n; ind1++) {
             for (int ind2= 1; ind2 <= m; ind2++) {
                 if (text1.charAt(ind1-1)==text2.charAt(ind2-1)){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    prev[ind2] = 1 + prev[ind2-1];
                 }else{
-                    dp[ind1][ind2] = Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    curr[ind2] = Math.max(prev[ind2],curr[ind2-1]);
                 }
             }
         }
-        return dp[n][m];
+        return prev[m];
     }
 
     public static void main(String[] args) {

3e4d3a88e10b9eb39793cbf82a201aab63712937
Adarshk18 <adarshkohli183@gmail.com>
2025-11-05 09:18:00 +0530

    new

---
+++ DynamicProgramming/src/ShortestCommonSubsequence.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class ShortestCommonSubsequence {
+
+    public static String shortestCommonSupersequence(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+
+        int i = n, j = m;
+        StringBuilder sb = new StringBuilder();
+
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+            } else {
+                sb.append(str2.charAt(j - 1));
+                j--;
+            }
+        }
+
+
+        while (i > 0) {
+            sb.append(str1.charAt(i - 1));
+            i--;
+        }
+
+        while (j > 0) {
+            sb.append(str2.charAt(j - 1));
+            j--;
+        }
+
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        while (t-- > 0) {
+            String text1 = in.next();
+            String text2 = in.next();
+
+            String result = shortestCommonSupersequence(text1, text2);
+            System.out.println(result);
+        }
+
+
+    }
+}

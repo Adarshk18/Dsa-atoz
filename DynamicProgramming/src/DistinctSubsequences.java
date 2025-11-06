84fc69952d10648e3540964c6a955c9894d9626f
Adarshk18 <adarshkohli183@gmail.com>
2025-11-06 08:39:48 +0530

    new

--- DynamicProgramming/src/DistinctSubsequences.java
+++ DynamicProgramming/src/DistinctSubsequences.java
@@ -6,30 +6,19 @@ public class DistinctSubsequences {
         int n = s.length();
         int m = t.length();
 
-        int[][] dp = new int[n+1][m+1];
-        for(int[] row: dp){
-            Arrays.fill(row,0);
-        }
+        int[] prev = new int[m + 1];
 
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-        }
 
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = 0;
-        }
+        prev[0] = 1;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                     dp[i][j] =  dp[i-1][j-1] + dp[i-1][j];
-                }else{
-                    dp[i][j] =   dp[i-1][j];
+            for (int j = m; j >= 1; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    prev[j] += prev[j - 1];
                 }
-
             }
         }
-        return dp[n][m];
+        return prev[m];
     }
 
     public static void main(String[] args) {
@@ -40,7 +29,7 @@ public class DistinctSubsequences {
             String s = in.next();
             String u = in.next();
 
-            int result = numDistinct(s,u);
+            int result = numDistinct(s, u);
             System.out.println(result);
         }
     }

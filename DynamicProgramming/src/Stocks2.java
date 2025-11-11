9a4bac2c91c40dded268ba1c45db444c69f3a453
Adarshk18 <adarshkohli183@gmail.com>
2025-11-11 09:55:23 +0530

    new

--- DynamicProgramming/src/Stocks2.java
+++ DynamicProgramming/src/Stocks2.java
@@ -1,28 +1,26 @@
 import java.util.*;
 
 public class Stocks2 {
-
-    public static int function(int[] prices, int ind, int buy, int[][] dp){
-        int n = prices.length;
-
-        if (ind==n) return 0;
-        if (dp[ind][buy]!=-1) return dp[ind][buy];
-
-        int profit =0;
-        if (buy==1){
-            profit = Math.max(-prices[ind] + function(prices,ind+1,0,dp), function(prices,ind+1,1,dp));
-        }else{
-            profit = Math.max(prices[ind] + function(prices,ind+1,1,dp),  function(prices,ind+1,0,dp));
-        }
-        return dp[ind][buy] = profit;
-    }
     public static int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n+1][2];
+        dp[n][0] = dp[n][1] = 0;
         for(int[] row: dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
-        return function(prices,0,1,dp);
+
+        for (int ind = n-1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit =0;
+                if (buy==1){
+                    profit = Math.max(-prices[ind] + dp[ind+1][0], dp[ind+1][1]);
+                }else{
+                    profit = Math.max(prices[ind] + dp[ind+1][1],  dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 
     public static void main(String[] args) {

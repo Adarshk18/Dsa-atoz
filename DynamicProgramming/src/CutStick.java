42c799e359d2b32e9652b2393411ca137f95547c
Adarshk18 <adarshkohli183@gmail.com>
2025-12-05 08:40:47 +0530

    new

--- DynamicProgramming/src/CutStick.java
+++ DynamicProgramming/src/CutStick.java
@@ -2,39 +2,59 @@ import java.util.*;
 
 public class CutStick {
 
-    public static int find(int i, int j, List<Integer> cut){
-        if (i>j) return 0;
-
-        int mini = Integer.MAX_VALUE;
-        for (int ind = i; ind <= j; ind++) {
-            int cost = cut.get(j+1)-cut.get(i-1) + find(i,ind-1,cut) + find(ind+1,j,cut);
-            mini = Math.min(mini,cost);
-        }
-        return mini;
-    }
-
     public static int minCost(int n, int[] cuts) {
-        List<Integer> cut = new ArrayList<>();
-        for(int c: cuts) cut.add(c);
 
-        cut.add(n);
+        List<Integer> cut = new ArrayList<>();
+
+        for (int c : cuts) cut.add(c);
+
         cut.add(0);
+        cut.add(n);
         Collections.sort(cut);
 
-        return find(1,cut.size()-2,cut);
+        int c = cut.size() - 2;
+
+        int[][] dp = new int[c + 2][c + 2];
+
+
+        for (int len = 1; len <= c; len++) {
+            for (int i = 1; i + len - 1 <= c; i++) {
+
+                int j = i + len - 1;
+
+                int mini = Integer.MAX_VALUE;
+
+                for (int ind = i; ind <= j; ind++) {
+
+                    int cost = (cut.get(j + 1) - cut.get(i - 1))
+                            + dp[i][ind - 1]
+                            + dp[ind + 1][j];
+
+                    mini = Math.min(mini, cost);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
+
+        return dp[1][c];
     }
+
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int t = in.nextInt();
-        while(t-->0){
+        while (t-- > 0) {
+
             int n = in.nextInt();
             int c = in.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = in.nextInt();
-            }
-            int len = minCost(c,arr);
-            System.out.println(len);
+
+            int[] arr = new int[c];
+            for (int i = 0; i < c; i++) arr[i] = in.nextInt();
+
+            int ans = minCost(n, arr);
+            System.out.println(ans);
         }
     }
 }
